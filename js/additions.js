bd532b0995c749049f5137ff187d30f57a340b03
Max Batt <mail@maxbatt.de>
2014-02-07 21:21:15 +0100

    Hover-Effekt in Bildergalerie eingebaut

--- js/additions.js
+++ js/additions.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
-	winH = ($(window).height());
+	
+	$('.box').hover(function(){
+		var clear =  $(this).children("div[data-clearimage]").data('clearimage');
+		//alert(clear);
+		$('.thumbnail-caption').css({
+			'background': 'url(' + clear + ')',
+			'background-repeat': 'no-repeat',
+			'background-size': '100%'
+		});
+	});
 	
 });
